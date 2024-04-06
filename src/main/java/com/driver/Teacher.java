d940f20ccf80cc4fc8bc0fd19a1f6f4e12e89226
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2024-04-07 01:59:40 +0530

    Update Teacher.java

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 public class Teacher {
@@ -8,31 +9,37 @@ public class Teacher {
 
     private int age;
 
+    public Teacher() {
 
-    public Teacher(String name,int numberOfStudents,int age){
-        this.name=name;
-        this.numberOfStudents=numberOfStudents;
-        this.age=age;
     }
 
-    public void setName(String name){
-        this.name=name;
-    }
-     public void setNumberOfStudents(int numberOfStudents){
-        this.numberOfStudents=numberOfStudents;
-    }
-    public void setAge(int age){
-        this.age=age;
+    public Teacher(String name, int numberOfStudents, int age) {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+        this.age = age;
     }
 
-    public String getName(){
-        return this.name;
+    public String getName() {
+        return name;
     }
-    public double getNumberOfStudents(){
-        return this.numberOfStudents;
+
+    public void setName(String name) {
+        this.name = name;
     }
-    public int getAge(){
-        return this.age;
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
     }
-  
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
