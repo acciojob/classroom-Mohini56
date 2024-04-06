333c44c2c269d9433f03e4d132f55b0893f01700
Mohini56 <91308740+Mohini56@users.noreply.github.com>
2024-04-07 01:57:48 +0530

    Update Student.java

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -6,32 +6,37 @@ public class Student {
     private int age;
     private double averageScore;
 
-    public Student(String name,int age,double averageScore){
-        this.name=name;
-        this.age=age;
-        this.averageScore=averageScore;
+    public Student(){
+
     }
 
-    public void setName(String name){
-        this.name=name;
-    }
-    public void setAge(int age){
-        this.age=age;
-    }
-    public void setAverage(double averageScore){
-        this.averageScore=averageScore;
+    public Student(String name, int age, double averageScore) {
+        this.name = name;
+        this.age = age;
+        this.averageScore = averageScore;
     }
 
-    public String getName(){
-        return this.name;
+    public String getName() {
+        return name;
     }
 
-    public int getAge(){
-        return this.age;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public double getAverage(){
-        return this.averageScore;
+    public int getAge() {
+        return age;
     }
-        
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public void setAverageScore(double averageScore) {
+        this.averageScore = averageScore;
+    }
 }
